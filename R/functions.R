7499eb320d3226f621e524470b7a2132365836dd
vegardlysne <vegard.lysne@gmail.com>
2022-09-21 15:45:30 +0200

    CompareOslo added

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,61 @@
+#' ReadFile
+#'
+#' @param file File name. Locates file by partial matching. 
+#' #' @param bank Either "NH" or "KH", defaults to "KH"
+#' @param year Either a 4-digit number corresponding to production year, or "DATERT". Defaults to DATERT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ReadFile <- function(file = NULL, 
+                     bank = "KH", 
+                     folder = "DATERT"){
+  
+  if(!(bank %in% c("KH", "NH"))) {
+    stop("`bank` must be either 'KH' or 'NH'")
+  }
+  
+  if(!(str_detect(folder, "[:digit:]{4}") & str_length(folder) == 4| 
+       folder == "DATERT")) {
+    stop("`folder` must be either 4 digits or 'DATERT'")
+  }
+  
+  if(is.null(file)) {
+    stop("file not selected")
+  }
+  
+  BANK <- case_when(bank == "KH" ~ "KOMMUNEHELSA",
+                    bank == "NH" ~ "NORGESHELSA")
+  
+  FOLDER <- case_when(folder == "DATERT" ~ paste0(folder, "/csv"),
+                      TRUE ~ paste0(bank, folder, "NESSTAR"))
+  
+  basepath <- file.path("F:", 
+                        "Forskningsprosjekter", 
+                        "PDB 2455 - Helseprofiler og til_",
+                        "PRODUKSJON", 
+                        "PRODUKTER", 
+                        "KUBER", 
+                        BANK, 
+                        FOLDER)
+  
+  filename <- list.files(basepath, pattern = file)
+  
+  if(length(filename) == 0){
+    stop("File not found, check spelling")
+  } else if(length(filename) > 1){
+    message("More than 1 file found:")
+    stop("Please specify file name to only select one file", 
+         cat(filename, sep = "\n"))
+  } else {
+    filepath <- file.path(basepath, filename)
+  }
+  
+  message(paste0("Loads file: ", BANK, "/", FOLDER, "/", basename(filepath)))
+  fread(filepath)
+}
+
 #' CompareDim
 #' 
 #' Detects all existing levels of selected dimension in the new KUBE, and compares towards a previous KUBE. The total number of levels, all new or expired levels, and a list of all existing levels in the new KUBE are listed in the output. 
@@ -140,11 +198,9 @@ CheckPrikk <- function(data1 = dfnew,
 #' @export
 #'
 #' @examples
-CompareLandFylke <- function(data1 = dfnew, groupdim = EXTRAdims, compare = COMPAREval){
+CompareLandFylke <- function(data1 = dfnew, groupdim = GROUPdims, compare = COMPAREval){
   
-  
-  
-  data1 %>% 
+  output <- data1 %>% 
     mutate(geolevel = case_when(GEO == 0 ~ "Land",
                                 GEO < 100 ~ "Fylke",
                                 GEO < 10000 ~ "Kommune",
@@ -156,7 +212,13 @@ CompareLandFylke <- function(data1 = dfnew, groupdim = EXTRAdims, compare = COMP
                 values_from = sum) %>% 
     mutate(absolute = Land-Fylke,
            relative = Land/Fylke) %>% 
-    arrange(desc(relative))
+    arrange(desc(relative)) 
+  
+  alwayslarger <- case_when(nrow(output %>% filter(relative < 1)) == 0 ~ "LAND is always larger than FYLKE",
+                            TRUE ~ "In some rows, FYLKE is larger than LAND")
+  
+  message(alwayslarger)
+  output
 }
 
 #' CompareBydelKommune
@@ -168,7 +230,7 @@ CompareLandFylke <- function(data1 = dfnew, groupdim = EXTRAdims, compare = COMP
 #' @export
 #'
 #' @examples
-CompareBydelKommune <- function(data1 = dfnew, groupdim = EXTRAdims, compare = COMPAREval) {
+CompareBydelKommune <- function(data1 = dfnew, groupdim = GROUPdims, compare = COMPAREval) {
   data1 %>% 
     mutate(geolevel = case_when(GEO == 0 ~ "Land",
                                 GEO < 100 ~ "Fylke",
@@ -185,5 +247,19 @@ CompareBydelKommune <- function(data1 = dfnew, groupdim = EXTRAdims, compare = C
     pivot_wider(names_from = geolevel, 
                 values_from = sum) %>% 
     mutate(absolute = Kommune-Bydel,
-           relative = Kommune/Bydel)
+           relative = Kommune/Bydel) %>% 
+    arrange(desc(relative))
+}
+
+CompareOslo <- function(data1 = dfnew, groupdim = GROUPdims, compare = COMPAREval){
+  data1 %>% 
+    filter(GEO %in% c("3", "301")) %>% 
+    mutate(geolevel = case_when(GEO == 3 ~ "Oslo Fylke",
+                                GEO == 301 ~ "Oslo Kommune")) %>% 
+    group_by(across(c(geolevel, all_of(groupdim)))) %>% 
+    summarise(sum = sum(.data[[compare]], na.rm = T), .groups = "drop") %>% 
+    pivot_wider(names_from = geolevel, 
+                values_from = sum) %>% 
+    mutate(absolute = `Oslo Fylke`-`Oslo Kommune`,
+           relative = `Oslo Fylke`/`Oslo Kommune`)
 }
--- R/setup.R
+++ R/setup.R
@@ -6,61 +6,6 @@ library(stringr)
 library(purrr)
 library(DT)
 
-#' ReadFile
-#'
-#' @param file File name. Locates file by partial matching. 
-#' #' @param bank Either "NH" or "KH", defaults to "KH"
-#' @param year Either a 4-digit number corresponding to production year, or "DATERT". Defaults to DATERT
-#'
-#' @return
-#' @export
-#'
-#' @examples
-ReadFile <- function(file = NULL, 
-                      bank = "KH", 
-                      year = "DATERT"){
-  
-  if(!(bank %in% c("KH", "NH"))) {
-    stop("`bank` must be either 'KH' or 'NH'")
-  }
-  
-  if(!(str_detect(year, "[:digit:]{4}") & str_length(year) == 4| 
-       year == "DATERT")) {
-    stop("`year` must be either 4 digits or 'DATERT'")
-  }
-  
-  if(is.null(file)) {
-    stop("file not selected")
-  }
-  
-  BANK <- case_when(bank == "KH" ~ "KOMMUNEHELSA",
-                    bank == "NH" ~ "NORGESHELSA")
-  
-  YEAR <- case_when(year == "DATERT" ~ paste0(year, "/csv"),
-                    TRUE ~ paste0(bank, year, "NESSTAR"))
-  
-  basepath <- file.path("F:", 
-                        "Forskningsprosjekter", 
-                        "PDB 2455 - Helseprofiler og til_",
-                        "PRODUKSJON", 
-                        "PRODUKTER", 
-                        "KUBER", 
-                        BANK, 
-                        YEAR)
-  
-  filename <- list.files(basepath, pattern = file)
-  
-  if(length(filename) == 0){
-    stop("File not found, check spelling")
-  } else if(length(filename) > 1){
-    message("More than 1 file found:")
-    stop("Please specify file name to only select one file", 
-         cat(filename, sep = "\n"))
-  } else {
-    filepath <- file.path(basepath, filename)
-  }
-  
-  message(paste0("Loads file: ", BANK, "/", YEAR, "/", basename(filepath)))
-  fread(filepath)
-}
+
+
 
